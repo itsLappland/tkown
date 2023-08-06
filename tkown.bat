df7c288d06853107bac37d5177bd25c58fc7962f
itsLappland <141420195+itsLappland@users.noreply.github.com>
2023-08-06 15:44:41 +0300

    v0.1.1

--- tkown.bat
+++ tkown.bat
@@ -1,5 +1,17 @@
 @echo off
 
+
+:: Check for elevated administrator rights
+
+:uac
+if exist %windir%\system32\config\systemprofile\* (
+  goto :tkown
+) else (
+  echo tkown does not have sufficient administrative rights. && exit /b 1
+)
+
+
+:tkown
 takeown /f %1 >nul
-icacls %1 /grant %username%:F >nul
+icacls %1 /grant "%username%":F >nul
 echo The file %1 now has your ownership.
